2cebe9e8e57fdccb4592dcb53caad828f90c1399
David Bloomin <daveey@gmail.com>
2022-10-04 20:19:25 -0700

    adds training scripts

--- setup/init_dev.sh
+++ setup/init_dev.sh
@@ -2,7 +2,10 @@ git clone https://github.com/daveey/platypus-nmmo-nips.git
 echo 'alias de="sudo docker exec -ti platypus /bin/bash"' >> ~/.bashrc
 echo 'alias db="sudo docker build -t platypus ."' >> ~/.bashrc
 echo 'alias dr="bash ~/platypus-nmmo-nips/setup/docker_run.sh"' >> ~/.bashrc
+echo 'alias dt="bash ~/platypus-nmmo-nips/setup/docker_train.sh"' >> ~/.bashrc
+echo 'alias dtc="bash ~/platypus-nmmo-nips/setup/docker_train_clean.sh"' >> ~/.bashrc
 echo 'alias ds="sudo docker stop platypus; sudo docker rm platypus"' >> ~/.bashrc
+
 source ~/.bashrc
 cd platypus-nmmo-nips
 
