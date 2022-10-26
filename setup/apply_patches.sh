ebb6161756398a7167d697ca4e50a36c11b77e8d
root <daveey@gmail.com>
2022-10-26 18:52:35 +0000

    fix patches

--- setup/apply_patches.sh
+++ setup/apply_patches.sh
@@ -1,3 +1,3 @@
-  130  cat ../setup/analyzer.patch | patch -R /root/miniconda3/envs/torchbeast/lib/python3.9/site-packages/neurips2022nmmo/evaluation/analyzer.py
-  140  cat ../setup/metrics.patch | patch -R /root/miniconda3/envs/torchbeast/lib/python3.9/site-packages/neurips2022nmmo/env/metrics.py
-  149  cat ../setup/tasks.patch | patch -R /root/miniconda3/envs/torchbeast/lib/python3.9/site-packages/neurips2022nmmo/tasks.py 
+cat ../setup/analyzer.patch | patch -R /root/miniconda3/envs/torchbeast/lib/python3.9/site-packages/neurips2022nmmo/evaluation/analyzer.py
+cat ../setup/metrics.patch | patch -R /root/miniconda3/envs/torchbeast/lib/python3.9/site-packages/neurips2022nmmo/env/metrics.py
+cat ../setup/tasks.patch | patch -R /root/miniconda3/envs/torchbeast/lib/python3.9/site-packages/neurips2022nmmo/tasks.py 
